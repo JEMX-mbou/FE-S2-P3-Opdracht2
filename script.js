bd96f760f043085ffaea36cf442821a38bc56c2d
jemx <jemx@mboutrecht.nl>
2022-02-18 09:50:49 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Maak een variabele aan genaamd: subtractButtons en selecteer alle buttons met de klasse subtract.
+// Maak een variabele aan genaamd: addButtons en selecteer alle buttons met de klasse add.
+
+// Maak een variabele aan genaamd: counterEl en selecteer het div element met het id counter.
+// Maak een variabele aan genaamd: counterVal en selecteer het innerHTML attribuut van de counterEl variabele.
+// - - Omdat counterVal een variabele is die moet veranderen moeten we "let" gebruiken in plaats van "const"
